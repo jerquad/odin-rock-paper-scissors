a2566eccb1ba5d6138802ac479240df2efb2379e
Jimmy <jimmyquadros@hotmail.com>
2022-08-23 10:32:45 -0400

    Streamlined getComputerChoice

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
 function getComputerChoice() {
     const rand = Math.floor(Math.random() * 3);
-    console.log(rand);
-    switch (true) {
-        case rand < 1:
-            return "rock";
-        case rand < 2:
-            return "paper";
-        default:
-            return "scissors";
+    return (rand < 1) ? "rock" : (rand < 2) ? "paper" : "scissors";
     }
-}
