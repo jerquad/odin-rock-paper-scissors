c8cf94da25c017ee1ab02b6f610795080ecba594
Jimmy <jimmyquadros@hotmail.com>
2022-08-23 12:28:10 -0400

    Add game functionality and streamline other functions

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
+/*
+    The Logic for a simple Rock Paper Scissors game created
+    for an assignment as part of The Odin Project
+*/
+
+// Randomly return one of three selections
 function getComputerChoice() {
     const rand = Math.floor(Math.random() * 3);
     return (rand < 1) ? "Rock" : (rand < 2) ? "Paper" : "Scissors";
     }
 
+/* 
+    Prompt the player for choice, case insensitive, requires 
+    correct choice, allows for singular scissor
+*/
 function getPlayerChoice() {
-    return selection = prompt("Rock Paper or Scissor?").toLowerCase();
+    let playerChoice;
+    while (true) {
+        playerChoice = prompt("Your Up! Rock, Paper, or Scissors?!");
+        playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1).toLowerCase();
+        if (playerChoice == "Scissor") {
+            playerChoice += "s";
+        }
+        if (playerChoice == "Rock" || playerChoice == "Paper" || playerChoice == "Scissors") {
+            break;
+        }
+    }
+    return playerChoice;
 }
 
+// Returns a victory condition based on passed strings
 function isWin(us, them) {
     switch (true) {
         case us == "Rock" && them == "Scissors":
@@ -20,15 +42,49 @@ function isWin(us, them) {
     }
 }
 
+// Compares two player selections determines win/loss/tie
 function playRound(playerChoice, computerChoice) {
-    playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1).toLowerCase();
     if (isWin(playerChoice, computerChoice)) {
-        console.log(`You Win! ${playerChoice} beats ${computerChoice}!`);
+        return "W";
     }
     else if (isWin(computerChoice, playerChoice)) {
-        console.log(`You Lose! ${computerChoice} beats ${playerChoice}!`);
+        return "L";
     }
     else {
-        console.log("It's a tie!")
+        return "T";
     }
+}
+
+// loops rounds of play until at least 3 points scored by either player
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let playerChoice, 
+        computerChoice;
+    for (let i = 0; i < 5; i++) {
+        playerChoice = getPlayerChoice();
+        computerChoice = getComputerChoice();
+        switch (playRound(playerChoice, computerChoice)) {
+            case "W":
+                console.log(`You Win! ${playerChoice} beats ${computerChoice}!`);
+                playerScore++;
+                break;
+            case "L":
+                console.log(`You Lose! ${computerChoice} beats ${playerChoice}!`);
+                computerScore++
+                break;
+            case "T":
+                console.log("It's a tie! Go Again!");
+                i--;
+                break;
+        }
+        console.log(`Player: ${playerScore}  Computer: ${computerScore}`);
+        if (playerScore == 3 || computerScore == 3) {
+            break;
+        }
+    }
+    const result = (playerScore > computerScore) 
+        ? "You Win! Great Job!" 
+        : "You Lose! Better Luck Next Tme!"
+    console.log(`FINAL: ${result}`);
 }
